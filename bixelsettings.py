abf9ab5a482461a5287a8a06e8f1b3597081def1
Donora <matthew.donora@gmail.com>
2020-02-26 12:47:06 +0000

    New Bixel Version building2

--- bixelsettings.py
+++ bixelsettings.py
@@ -4,14 +4,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 import time
 from datetime import datetime
-import RPi.GPIO as GPIO
 
-#Set up pin for fan control
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
-fanstatus = 'Off'
-
+#####################
 #Flags
 dicts = {
 'RunProtocol' : 0,
@@ -20,17 +14,29 @@ dicts = {
 'BlueTime' : 0.0,
 'GreenTime' : 0.0,
 'Duration' : 0,
-'SetTemp' : 0.0}
+'SetTemp' : 0.0,
+'TimeStart' : 'null',
+'DateStart' : 'null'}
 #Duration is in days; set to 0 for no set ending
 #Green time in seconds
 #Blue time in minutes
+
+#internal flag to avoid end of experiment by manual
+#navigation to /end page
+endroute = 0
+
+#internal flag to notify user that protocol was not successfully
+#written to config file (and therefore did not start)
+startfailflag = 0
+
+#####################
 
 #Read or create config file
 if os.path.exists("config.txt"):
     try:
         with open('config.txt', 'r') as config:
             for line in config:
-                if len(line) > 0:
+                if len(line) > 3:
                     (key, val) = line.strip("\n").split(',')
                     dicts[key] = val
             print("read config for initial setup")
@@ -48,20 +54,24 @@ def readfromconfig():
     try:
         with open('config.txt', 'r') as config:
             for line in config:
-                if len(line) > 0:
+                if len(line) > 3:
                     (key, val) = line.split(',')
                     dicts[key] = val
             print("read from config (first try)")
     except:
-        time.sleep(1)
-        with open('config.txt', 'r') as config:
-            for line in config:
-                if len(line) > 0:
-                    (key, val) = line.split(',')
-                    dicts[key] = val
-            print("read from config (second try)")
+        try:
+            print("failed first read; trying again")
+            time.sleep(1)
+            with open('config.txt', 'r') as config:
+                for line in config:
+                    if len(line) > 3:
+                        (key, val) = line.split(',')
+                        dicts[key] = val
+                print("read from config (second try)")
+        except:
+            print("failed on second try as well")
 
-
+#Write to config file
 def writetoconfig():
     global dicts
     try:
@@ -71,9 +81,6 @@ def writetoconfig():
             print("wrote to config")
     except:
         print(" config file opening failed (writetoconfig)")
-
-current_milli_time = lambda: int(round(time.time() * 1000))
-startmilli = current_milli_time()
 
 def gentime():
     now = datetime.now() #Get the date+time right now
@@ -89,22 +96,22 @@ def gettemphum():
     global hum
     hum = ht[0]
 
-timestart = 'unset'
-datestart = 'unset'
-
 @app.route("/")
 def index():
+ #internal flags
+    global endroute
+    endroute = 0
+#
     global dicts
-    global timestart
-    global datestart
     gettemphum()
     gentime()
     readfromconfig()
     templateData = {
     'title' : 'Home',
     'RunProtocol' : int(dicts['RunProtocol']),
-    'timestart' : timestart,
-    'datestart' : datestart,
+    'LockParameters' : int(dicts['LockParameters']),
+    'timestart' : dicts['TimeStart'],
+    'datestart' : dicts['DateStart'],
     'temp' : temp,
     'hum' : hum,
     'date' : date,
@@ -114,6 +121,10 @@ def index():
 
 @app.route("/configure")
 def configure():
+#internal flags
+    global endroute
+    endroute = 0
+#
     global dicts
     readfromconfig()
     gettemphum()
@@ -135,6 +146,12 @@ def configure():
 
 @app.route("/confirm", methods=['GET', 'POST'])
 def confirm():
+#internal flags
+    global endroute
+    global startfailflag
+    startfailflag = 0
+    endroute = 0
+#
     global dicts
     readfromconfig()
     dicts['RunProtocol'] = 1
@@ -144,36 +161,40 @@ def confirm():
     dicts['GreenTime'] = float(request.form['GreenTime']) #seconds
     dicts['Duration'] = float(request.form['Duration']) #days
     dicts['SetTemp'] = float(request.form['SetTemp'])
+    dicts['TimeStart'] = timenow
+    dicts['DateStart'] = date
     try:
         writetoconfig()
     except:
         dicts['RunProtocol'] = 0
+        dicts['LockParameters'] = 0
+        print("failed to write to config during parameter dump.")
+        startfailflag = 1
     gentime()
-    global timestart
-    global datestart
-    timestart = timenow
-    datestart = date
     gettemphum()
     templateData= {
         'title' : 'Confirm Protocol',
         'RunProtocol' : int(dicts['RunProtocol']),
         'temp' : temp,
+        'startfailflag' : startfailflag,
         'hum' : hum,
         'date' : date,
         'time' : timenow,
+        'timestart' : dicts['TimeStart'],
+        'datestart' : dicts['DateStart'],
         'BlueTime' : float(dicts['BlueTime']),
         'GreenTime' : float(dicts['GreenTime']),
         'Duration' : float(dicts['Duration']),
-        'SetTemp' : float(dicts['SetTemp']),
+        'SetTemp' : float(dicts['SetTemp'])
     }
 
     return render_template('confirm.html', **templateData)
 
 @app.route("/view")
 def view():
+    global endroute
+    endroute = 0
     global dicts
-    global timestart
-    global datestart
     gentime()
     gettemphum()
     readfromconfig()
@@ -181,18 +202,61 @@ def view():
     templateData= {
         'title' : 'Data View',
         'temp' : temp,
-        'timestart' : timestart,
-        'datestart' : datestart,
+        'timestart' : dicts['TimeStart'],
+        'datestart' : dicts['DateStart'],
         'hum' : hum,
         'date' : date,
         'time' : timenow,
         'RunProtocol' : int(dicts['RunProtocol']),
+        'LockParameters' : int(dicts['LockParameters']),
+        'BlueTime' : float(dicts['BlueTime']),
+        'GreenTime' : float(dicts['GreenTime']),
+        'Duration' : float(dicts['Duration']),
+        'SetTemp' : float(dicts['SetTemp'])
+    }
+    return render_template('view.html', **templateData)
+
+@app.route("/endconfirm")
+def endconfirm():
+    global endroute
+    endroute = 1
+    templateData= {
+        'title' : 'Confirm End Protocol',
+    }
+    return render_template('endconfirm.html', **templateData)
+
+@app.route("/end")
+def end():
+    global endroute
+    global dicts
+    gentime()
+    gettemphum()
+    readfromconfig()
+#FIND IMAGES for etc
+    if endroute == 1:
+        dicts['EndProtocol'] = 1
+        dicts['StartProtocol'] = 0
+        dicts['LockParameters'] = 0
+        writetoconfig()
+    templateData= {
+        'endroute' : endroute,
+        'title' : 'End Protocol',
+        'temp' : temp,
+        'timestart' : dicts['TimeStart'],
+        'datestart' : dicts['DateStart'],
+        'hum' : hum,
+        'date' : date,
+        'time' : timenow,
+        'RunProtocol' : int(dicts['RunProtocol']),
+        'RunProtocol' : int(dicts['RunProtocol']),
+        'LockParameters' : int(dicts['LockParameters']),
         'BlueTime' : float(dicts['BlueTime']),
         'GreenTime' : float(dicts['GreenTime']),
         'Duration' : float(dicts['Duration']),
         'SetTemp' : float(dicts['SetTemp']),
     }
-    return render_template('view.html', **templateData)
+    endroute = 0
+    return render_template('end.html', **templateData)
 
 
 
